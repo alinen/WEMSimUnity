074866cbc2e379743166c85f87fcf25ab6ad4ad3
alinen <nenila@gmail.com>
2018-12-13 12:39:20 -0500

    initialize project for WEMSim

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+    public float rate = 1.0f;
+    Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+        Debug.Log("HERE!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = startPosition + Mathf.Sin(Time.timeSinceLevelLoad * rate)*(new Vector3(0,1,0));
+		
+	}
+}
---
+++ Assets/Scripts/SphereCrowd.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SphereCrowd : MonoBehaviour {
+
+    public int numSpheres;
+    public GameObject sphereTemplate;
+
+	// Use this for initialization
+	void Start () {
+        System.Random random = new System.Random();
+        for (int i = 0; i < numSpheres; i++) {
+            GameObject copy = GameObject.Instantiate(sphereTemplate);
+            float x = random.Next(-5,5);
+            float z = random.Next(-5,5);
+            copy.transform.position = new Vector3(x,0,z);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
